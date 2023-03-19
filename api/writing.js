cea50f65a3226158b3f5041f47fc4d15638fcb37
Dowol <dowol1212@gmail.com>
2023-03-19 23:35:30 +0900

    Apply MySQL Transaction

--- api/writing.js
+++ api/writing.js
@@ -2,31 +2,54 @@ const {Router} = require("express") //express 안에 있는것 중에서 Router 
 const pool = require("../DBconnection")
 const router = Router()
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
     var connection = await pool.getConnection()
-    
+
     var nickname = req.body.nickname
     var letter = req.body.letter_context
     var food_name = req.body.fEname
 
-    console.log(nickname, letter, food_name)
+    try {
+        await connection.beginTransaction();
 
-    let [check_duplicate] = await connection.query('SELECT COUNT(name) AS duplicate FROM food_db.submitlist WHERE name=?', nickname);
-    console.log(check_duplicate)
-
-    if(check_duplicate[0]?.duplicate){
-        res.status(400).json({
-            result: 'fail',
-            reason: 'DUPLICATE_ITEM',
-            message: '호패가 중복되었소. 다른 호패로 변경해주오.'
-        });
-    }
-    else {
         await connection.query('INSERT into submitlist(food_name, name, content) values(?, ?, ?);', [food_name, nickname, letter])
 
-        res.status(200).json({
-            result: 'success'
-        });
+        let [sel_result] = await connection.query('SELECT * FROM submitlist WHERE `name` = ?', nickname);
+
+        if(sel_result.length === 1 && food_name === sel_result[0].food_name){
+            res.status(200).json({
+                result: 'success'
+            });
+        }
+        else{
+            throw {
+                reason: 'DB_VALIDATION_FAILED'
+            }
+        }
+
+    } catch (e) {
+        connection.rollback();
+
+        let body = {
+            result: 'fail',
+            reason: 'UNKNOWN',
+            message: '예기치 않은 문제로 등록에 실패하였소.'
+        }
+        if(e.code){
+            switch (e.code) {
+                case 'ER_DUP_ENTRY':
+                    body.reason = 'DUPLICATE_ITEM';
+                    body.message = '호패가 중복되었소. 다른 호패로 변경해주오.';
+                    break;
+            }
+        }
+        else {
+            body = Object.assign(body, e);
+        }
+
+        res.status(400).json(body);
+    } finally {
+        connection.release();
     }
 })
 
